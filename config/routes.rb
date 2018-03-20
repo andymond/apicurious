6ae3d545b27434f7f1d1d8416a4df877278904b9
Andrew Dymond <andy.dymond@gmail.com>
2018-03-19 19:27:33 -0600

    Basic github oauth implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "/auth/github", as: :github_login
+  get "/auth/github/callback", to: "sessions#create"
   resources :dashboard, only: [:index]
 end
---
+++ db/migrate/20180320011058_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :screenname
+      t.string :uid
+      t.string :oauth_token
+      t.string :oauth_token_secret
+
+      t.timestamps
+    end
+  end
+end
