ef6235ea35e18e5cfdf21b50440d2d165c3a431b
Andrew Dymond <andy.dymond@gmail.com>
2018-03-20 18:24:10 -0600

    Add user_can_view_basic_github_info_spec

---
+++ spec/features/user_can_view_basic_github_info_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+feature "user visits dashboard" do
+  it "displays github profile info" do
+    user = User.create(oauth_token: ENV['GITHUB_ACCESS_TOKEN'])
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit dashboard_index_path
+
+    expect(page).to have_content("Andy Dymond")
+    expect(page).to have_content("andymond")
+    expect(page).to have_content("Denver, CO")
+    expect(page).to have_content("andy.dymond@gmail.com")
+    expect(page).to have_content("Repositories 41")
+    expect(page).to have_content("Stars 1")
+    expect(page).to have_content("Followers 3")
+    expect(page).to have_content("Following 4")
+  end
+end
