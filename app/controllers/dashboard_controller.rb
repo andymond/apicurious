b1bf61d6e7e77097093980ba04fb26856eb4dd2d
Andrew Dymond <andy.dymond@gmail.com>
2018-03-20 19:18:17 -0600

    Add GithubUser & api call to dashboard_controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,8 @@
 class DashboardController < ApplicationController
 
   def index
-
+    response = Faraday.get("https://api.github.com/user?access_token=#{current_user.oauth_token}")
+    @user = GithubUser.new(response.body)
   end
 
 end
