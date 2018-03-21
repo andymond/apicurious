db5a6bdf097e478321dca327342204ad8cca5c54
Andrew Dymond <andy.dymond@gmail.com>
2018-03-20 19:43:29 -0600

    Add @user attributes to dashboard/index, add stars to controller and github_user model

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,9 @@
 class DashboardController < ApplicationController
 
   def index
-    response = Faraday.get("https://api.github.com/user?access_token=#{current_user.oauth_token}")
-    @user = GithubUser.new(response.body)
+    user_response = Faraday.get("https://api.github.com/user?access_token=#{current_user.oauth_token}")
+    stars_response = Faraday.get("https://api.github.com/user/starred?access_token=#{current_user.oauth_token}")
+    @user = GithubUser.new(user_response.body, stars_response.body)
   end
 
 end
--- app/models/github_user.rb
+++ app/models/github_user.rb
@@ -1,8 +1,15 @@
 class GithubUser
-  attr_reader :attributes, :location, :bio, :repos, :followers, :following
+  attr_reader :attributes,
+              :stars,
+              :location,
+              :bio,
+              :repos,
+              :followers,
+              :following
 
-  def initialize(json)
-    @attributes = JSON.parse(json, symbolize_names: true)
+  def initialize(user_info, user_stars = nil)
+    @attributes = JSON.parse(user_info, symbolize_names: true)
+    @stars = JSON.parse(user_stars, symbolize_names: true)
     @location = attributes[:location]
     @bio = attributes[:bio]
     @repos = attributes[:public_repos]
