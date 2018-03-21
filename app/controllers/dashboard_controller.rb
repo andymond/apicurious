55eb1e2ad2e92e82162dfde49f002778101b1e69
Andrew Dymond <andy.dymond@gmail.com>
2018-03-21 14:33:18 -0600

    Refactor github_user model & controller so controller doesn't call service

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,7 @@
 class DashboardController < ApplicationController
 
   def index
-    github_service = GithubService.new(current_user)
-    user_info = github_service.user_info
-    stars = github_service.user_stars
-    @user = GithubUser.new(user_info, stars)
+    @user = GithubUser.new(current_user)
   end
 
 end
--- app/models/github_user.rb
+++ app/models/github_user.rb
@@ -1,20 +1,23 @@
 class GithubUser
   attr_reader :attributes,
-              :stars,
               :location,
               :bio,
               :repos,
               :followers,
               :following
 
-  def initialize(attributes, stars)
-    @attributes = attributes
-    @stars = stars
+  def initialize(user)
+    @gh_service = GithubService.new(user)
+    @attributes = @gh_service.user_info
     @location = attributes[:location]
     @bio = attributes[:bio]
     @repos = attributes[:public_repos]
     @followers = attributes[:followers]
     @following = attributes[:following]
+  end
+
+  def stars
+    @gh_service.user_stars
   end
 
 end
