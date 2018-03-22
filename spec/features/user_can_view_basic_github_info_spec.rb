89cf8a6c9f74679e4f87a56585e6a0baff933cba
Andrew Dymond <andy.dymond@gmail.com>
2018-03-22 10:58:19 -0600

    refactor user_can_view_basic_github_info_spec to work with webmock

--- spec/features/user_can_view_basic_github_info_spec.rb
+++ spec/features/user_can_view_basic_github_info_spec.rb
@@ -3,6 +3,12 @@ require "rails_helper"
 feature "user visits dashboard" do
   it "displays github profile info from api" do
     user = User.create(oauth_token: ENV['GITHUB_ACCESS_TOKEN'])
+    user_url = "https://api.github.com/user?access_token=#{user.oauth_token}"
+    starred_url = "https://api.github.com/user/starred?access_token=#{user.oauth_token}"
+
+    stub_get("user_info.json", user_url)
+    stub_get("starred_info.json", starred_url)
+
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit dashboard_index_path
