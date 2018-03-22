e6b05aaab48b8373cdef63b473af031e573899d5
Andrew Dymond <andy.dymond@gmail.com>
2018-03-22 14:04:02 -0600

    Add github_event_service_spec

---
+++ spec/services/github_event_service_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+describe GithubEventService do
+  let(:user) { double(oauth_token: ENV['GITHUB_ACCESS_TOKEN'], nickname: "andymond") }
+  let(:service) { GithubEventService.new(user, 1) }
+
+  it "returns json of recent user activity" do
+    VCR.use_cassette("event_info") do
+      user_events = service.user_events
+
+      expect(user_events).to be_an(Array)
+      expect(user_events.count).to eq(30)
+      expect(user_events.first).to have_key(:type)
+      expect(user_events.first).to have_key(:actor)
+      expect(user_events.first).to have_key(:repo)
+      expect(user_events.first).to have_key(:payload)
+      expect(user_events.first).to have_key(:public)
+      expect(user_events.first).to have_key(:created_at)
+    end
+  end
+end
--- spec/services/github_repo_service_spec.rb
+++ spec/services/github_repo_service_spec.rb
@@ -4,7 +4,7 @@ describe GithubRepoService do
   let(:user) { double(oauth_token: ENV['GITHUB_ACCESS_TOKEN'], nickname: "andymond") }
   let(:service) { GithubRepoService.new(user, 1) }
 
-  it "returns user information from GithubApi" do
+  it "returns user repository information from GithubApi" do
     VCR.use_cassette("repo_info") do
       repos = service.user_repos
 
